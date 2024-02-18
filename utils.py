51c0d37febd63f4cc84dfd418085e1f740ab77f3
Pirant0409 <ant.mau.pir@gmail.com>
2024-02-18 10:04:46 +0100

    updating name format

--- utils.py
+++ utils.py
@@ -5,17 +5,19 @@ import speech_text_speech as stt
 from io import BytesIO
 from pydub import AudioSegment
 
-def hexToMp3(audioString):
+def hexToMp3(audioString,i):
 
-    i = 0
     newAudioString = base64.b64decode(audioString)
     print("Decodage de l'audio")
     audio_bytes_io = BytesIO(newAudioString)
     print("Conversion de l'audio en mp3")
     audio = AudioSegment.from_file(audio_bytes_io)
-    while(os.path.exists("./audio/output"+str(i)+".wav")):
-        i+=1
-    audio.export("./audio/output"+str(i)+".wav", format="wav")
+    if os.listdir("./audio") == []:
+        i = 0
+    else:
+        files_list = os.listdir("./audio")
+        i = int(files_list[-1].split("_")[0]) + 1
+    audio.export("./audio/"+str(i)+"_output.wav", format="wav")
     print("Audio créé avec succès")
     stt.process_audio_files("./audio", "./audio_translated")
     print("Audio traité avec succès")
